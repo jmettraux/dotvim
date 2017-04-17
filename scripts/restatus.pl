0657357cbff3a0df0334b2aaa0945719a567d3ac
John Mettraux <jmettraux@gmail.com>
2017-04-17 11:16:57 +0900

    Enhance ListFiles, put git change count at end
    ```
    spec/parser.md | ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 79
    ```
    instead of
    ```
    spec/parser.md | 79 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    ```
    
    So that hitting <ENTER> doesn't open parser.md at line 79...

--- scripts/restatus.pl
+++ scripts/restatus.pl
@@ -5,8 +5,8 @@ while (<>) {
   if (/\A\s+new file:\s+(.+)\n/) {
     print $1 . "  (new file)\n";
   }
-  elsif (/\A ([^\(]+\s+| \d+ [-+]+)\z/) {
-    print $1 . "\n";
+  elsif (/\A ([^\(]+\s+| )(\d+) ([-+]+)/) {
+    print $1 . $3 . " " . $2 . "\n";
   }
   elsif (/\A\s\d+ /) {
     print "#\n#" . $_;
