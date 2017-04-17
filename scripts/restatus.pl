fe519d8c447c45f42cf4dd2207cf17aad1e832c0
John Mettraux <jmettraux@gmail.com>
2017-04-17 17:34:17 +0900

    Fix restatus.pl vs git diff

--- scripts/restatus.pl
+++ scripts/restatus.pl
@@ -2,11 +2,12 @@
 # MIT Licensed
 
 while (<>) {
+
   if (/\A\s+new file:\s+(.+)\n/) {
     print $1 . "  (new file)\n";
   }
-  elsif (/\A ([^\(]+\s+| )(\d+) ([-+]+)/) {
-    print $1 . $3 . " " . $2 . "\n";
+  elsif (/\A ([^\(]+\s+\|)[ ]*(\d+) ([-+]+)/) {
+    print $1 . ' ' . $3 . ' ' . $2 . "\n";
   }
   elsif (/\A\s\d+ /) {
     print "#\n#" . $_;
