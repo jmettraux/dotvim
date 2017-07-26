42479a8c5cc0a725d5636e788dc61a558b6eefb1
John Mettraux <jmettraux@gmail.com>
2017-07-26 15:14:01 +0900

    Let Scan recognize Javascript functions

--- scripts/scan.pl
+++ scripts/scan.pl
@@ -3,7 +3,7 @@
 
 $i = 1;
 while (<>) {
-  if (/^\s*(module\s+|class\s+|def\s+[^\s.]+|\bfunction\b)/) {
+  if (/^\s*(module\s+|class\s+|def\s+[^\s.]+|\bvar\s+.+\s+=\s+\(?function\b)|\s+this\.\S+\s+=\s+function\b/) {
     printf("%5d %s", $i, $_);
   };
   $i = $i + 1;
