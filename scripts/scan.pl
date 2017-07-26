ca4b2dee5b6f9edf2b32b303d9f9785f3c93f1cd
John Mettraux <jmettraux@gmail.com>
2017-07-27 05:45:28 +0900

    Scan for public/protected/private

--- scripts/scan.pl
+++ scripts/scan.pl
@@ -4,13 +4,16 @@
 $i = 1;
 while (<>) {
   if (
-    /^\s*(
-      module\s+|
-      class\s+|
-      def\s+[^\s.]+|
-      \bvar\s+.+\s+=\s+\(?function\b
-    )|
-    \s+this\.\S+\s+=\s+function\b/x
+    /^\s*
+      (
+        module\s+|
+        class\s+|
+        def\s+[^\s.]+|
+        \bvar\s+.+\s+=\s+\(?function\b|
+        public\b|protected\b|private\b
+      )|
+      \s+this\.\S+\s+=\s+function\b
+    /x
   ) {
     printf("%5d %s", $i, $_);
   };
