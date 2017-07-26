6619268eb610060058da0f74d0b207e890f1939d
John Mettraux <jmettraux@gmail.com>
2017-07-27 05:41:39 +0900

    Explode scan.pl regex pattern on multiple lines

--- scripts/scan.pl
+++ scripts/scan.pl
@@ -3,7 +3,15 @@
 
 $i = 1;
 while (<>) {
-  if (/^\s*(module\s+|class\s+|def\s+[^\s.]+|\bvar\s+.+\s+=\s+\(?function\b)|\s+this\.\S+\s+=\s+function\b/) {
+  if (
+    /^\s*(
+      module\s+|
+      class\s+|
+      def\s+[^\s.]+|
+      \bvar\s+.+\s+=\s+\(?function\b
+    )|
+    \s+this\.\S+\s+=\s+function\b/x
+  ) {
     printf("%5d %s", $i, $_);
   };
   $i = $i + 1;
