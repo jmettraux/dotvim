0435d0e7299c4731acaad8035e78e662a105f807
John Mettraux <jmettraux@gmail.com>
2017-12-11 09:48:00 +0900

    Explore restatus.pl rework

--- scripts/restatus.pl
+++ scripts/restatus.pl
@@ -3,9 +3,13 @@
 
 while (<>) {
 
+#print ">>>" . $_;
   if (/\A\s+new file:\s+(.+)\n/) {
     print $1 . "  (new file)\n";
   }
+  #elsif (/\A\s+modified:\s+(.+)\n/) {
+  #  print $1 . "  (modified)\n";
+  #}
   elsif (/\A ([^\(]+\s+\|)[ ]*(\d+) ([-+]+)/) {
     print $1 . ' ' . $3 . ' ' . $2 . "\n";
   }
