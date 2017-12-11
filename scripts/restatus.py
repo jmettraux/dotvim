c5717c0acbe9af522658e8182f4eeb6511287067
John Mettraux <jmettraux@gmail.com>
2017-12-11 18:30:48 +0900

    Fix restatus.py modified/deleted/new regexes

--- scripts/restatus.py
+++ scripts/restatus.py
@@ -9,24 +9,29 @@ paths = {}
 footer = []
 
 for line in lines:
-  m = re.match('\A\s+new file:\s+(.+)\n', line)
+  m = re.match('\A\s+new file:\s+(.+)', line)
   if m:
     paths[m.group(1)] = m.group(1) + "  (new file)"
     continue
-  m = re.match('\A\s+modified:\s+(.+)\n', line)
+  m = re.match('\A\s+modified:\s+(.+)', line)
   if m:
     paths[m.group(1)] = m.group(1) + "  (modified)"
     continue
-  m = re.match('\A ([^\(]+\s+\|)[ ]*(\d+) ([-+]+)', line)
+  m = re.match('\A\s+deleted:\s+(.+)', line)
   if m:
-    paths[m.group(1)] = m.group(1) + " " + m.group(3) + " " + m.group(2)
+    paths[m.group(1)] = m.group(1) + "  (deleted)"
+    continue
+  m = re.match('\A ([^\(]+)\s+\|[ ]*(\d+) (\+[-+]*)', line)
+  if m:
+    paths[m.group(1)] = m.group(1) + " | " + m.group(3) + " " + m.group(2)
     continue
   m = re.match('\A\s\d+ ', line)
   if m:
     footer.append("#\n#" + line)
     #continue
+  #print '>>>' + line
 
-for line in paths.values():
+for line in sorted(paths.values()):
   print line
 for line in footer:
   print line
