2a8c8892774a0e23323efc7924a55a5dad0b9388
John Mettraux <jmettraux@gmail.com>
2018-10-12 14:31:47 +0900

    Enhance buffers.py, show buffer number

--- scripts/buffers.py
+++ scripts/buffers.py
@@ -6,9 +6,28 @@ import os, re, sys
 
 print "== buffers"
 
+normals = []
+specials = []
+
 for line in sys.stdin:
   m = re.match(r'^(\d+)([^"]*)"([^"]+)"\s+line (\d+)', line.strip())
   if not(m):
     continue
-  print m.group(3) + ':' + m.group(4) + '  ' + m.group(1)
+  l = [ m.group(3) + ':' + m.group(4), m.group(1).rjust(3) ]
+  if re.search(r'^_[a-z]___[a-zA-Z0-9_]', l[0]):
+    specials.append(l)
+  else:
+    normals.append(l)
+
+mx = 0
+  #
+for l in normals:
+  mx = max(len(l[0]), mx)
+for l in specials:
+  mx = max(len(l[0]), mx)
+
+for l in normals:
+  print l[0].ljust(mx) + l[1]
+for l in specials:
+  print l[0].ljust(mx) + l[1]
 
