267ea8daa36cd13012d66f6cc14979737a6ee282
John Mettraux <jmettraux@gmail.com>
2019-05-17 13:54:56 +0900

    Harden buffer listing against path/nada (1).txt

--- scripts/recentfiles.py
+++ scripts/recentfiles.py
@@ -7,11 +7,18 @@ rejects = [
   'COMMIT_EDITMSG', 'NetrwTreeListing', 'bash-fc-', '==[A-Z]',
   '\/private\/var\/', '\/mutt-' ]
 
+
 def expand_path(path):
   p = os.path.relpath(os.path.expanduser(path))
   if re.match('\.\.\/\.\.\/', p):
     p = os.path.abspath(p)
   return p
+
+# escape spaces and parentheses
+#
+def escape_path(path):
+  return re.sub(r'([ \(\)])', r'\\\1', path)
+
 
 paths = []
   #
@@ -24,6 +31,7 @@ for line in sys.stdin:
   path = expand_path(m.group(1))
   if not os.path.isfile(path):
     continue
+  path = escape_path(path)
   paths.append(path)
 
 tree = { '/': [], '..': [], '.': {} }
@@ -46,50 +54,14 @@ d = {}
 cmd = 'ls -lh ' + string.join(paths)
   #
 for line in subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout:
-  m = re.match('^.+ +.+ +.+ +.+ ([0-9.]+[BKMT]?) +.+ \d+ +[0-9:]+ +(.+)$', line)
+  m = re.match(r'^.+ +.+ +.+ +.+ ([0-9.]+[BKMT]?) +.+ \d+ +[0-9:]+ +(.+)$', line)
   if m:
-    d[m.group(2)] = m.group(2) + ' ' + m.group(1)
+    path = escape_path(m.group(2))
+    d[path] = m.group(2) + ' ' + m.group(1)
 for path in paths:
   l = d.get(path, None)
   if l:
     print l
-
-#print "== recent (tree)"
-#
-#def do_print_level(a, h, k):
-#  v = h[k]
-#  if len(v) > 0:
-#    print "%s %s/" % (''.join(a), k)
-#    aa = a[0:-1]
-#    aa.append("│  ")
-#    aa.append("├─")
-#    print_level(aa, v)
-#  else:
-#    print "%s %s" % (''.join(a), k)
-#
-#def print_level(a, h):
-#  ks = h.keys()
-#  ks.sort()
-#  for k in ks[0:-1]:
-#    do_print_level(a, h, k)
-#  for k in ks[-1:]:
-#    aa = a[:-1]
-#    aa.append("└─")
-#    do_print_level(aa, h, k)
-#
-#print './'
-#print_level([ "├─" ], tree['.'])
-#
-#def print_flat(h, k):
-#  if len(h[k]) > 0:
-#    print (k if k == '/' else "%s/" % k)
-#    off = 1 if k == '/' else len(k) + 1
-#    for path in h[k][0:-2]:
-#      print "├── %s" % path[off:]
-#    for path in h[k][-2:-1]:
-#      print "└── %s" % path[off:]
-#
-#print_flat(tree, '..')
-##print_flat(tree, '/')
-#print "│"
+  #else:
+  #  print "NOT FOUND: " + path
 
