cca0d858272270bcd922f18dc6c535b878afbc56
John Mettraux <jmettraux@gmail.com>
2020-04-20 09:59:43 +0900

    Make file git diff lnum col narrower

--- scripts/opengitdiff.py
+++ scripts/opengitdiff.py
@@ -4,22 +4,28 @@
 
 import sys, re, subprocess
 
-path = sys.argv[1]
+lines = subprocess\
+  .Popen(
+    'git diff -U9999999 --no-color ' + sys.argv[1],
+    shell=True,
+    stdout=subprocess.PIPE)\
+  .stdout\
+  .readlines()
 
-cmd = 'git diff -U9999999 --no-color ' + path
+digits = str(len(str(len(lines))))
 
 lnum = -1
 
-for line in subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout:
+for line in lines:
 
-  #print line.rstrip()
+  line = line.rstrip('\r\n')
 
   if lnum > -1:
     if re.match(r'^[^-]', line): lnum = lnum + 1
-    print '%5i %s' % (lnum, line.rstrip())
+    print ('%' + digits + 'i %s') % (lnum, line)
   else:
-    print line.rstrip()
-    m = re.match(r'^@@ [-+0-9, ]+ @@\n', line)
+    print line
+    m = re.match(r'^@@ [-+0-9, ]+ @@$', line)
     if m:
       lnum = 0
       print
