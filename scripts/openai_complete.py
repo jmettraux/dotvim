7ed8ecc773bffba3c07a68da35ea388cbfe788e4
John Mettraux <jmettraux@gmail.com>
2023-04-09 15:52:44 +0900

    Try not to send too many tokens to OpenAI

--- scripts/openai_complete.py
+++ scripts/openai_complete.py
@@ -1,10 +1,19 @@
 
 # scripts/openai.py
 
-import os, sys
+import os, sys, re
 import json
 import openai
 from pathlib import Path
+
+
+# primitive but...
+#
+def count_tokens(s):
+  return \
+    len(re.findall(r'\w+', s)) + \
+    len(re.findall(r'\s', s)) + \
+    len(re.findall(r'[^\w\s]', s))
 
 
 with open(os.path.expanduser('~') + '/.vim/.openai.key.txt', 'r') as file:
@@ -13,9 +22,11 @@ with open(os.path.expanduser('~') + '/.vim/.openai.key.txt', 'r') as file:
 fname_last = '.openai.last.py'
 fname_messages = '.openai.messages.py'
 model = 'gpt-3.5-turbo'
-max_tokens = 1400
 temperature = 0.7
-max_messages = 2 * 7
+
+whole_tokens = 4096
+max_tokens = 1400
+rem_tokens = whole_tokens - max_tokens
 
 lines = sys.stdin.read().strip()
 
@@ -23,17 +34,25 @@ Path(fname_messages).touch()
 
 prompt = { "role": "user", "content": lines }
 
-messages = []
+all_messages = []
   #
 with open(fname_messages) as f:
   while line := f.readline():
-    messages.append(json.loads(line.strip()))
+    all_messages.append(json.loads(line.strip()))
   #
-messages.append(prompt)
-messages = messages[-max_messages:]
+all_messages.append(prompt)
 
-#response = openai.Completion.create(model="ada", prompt="Hello world")
-#print(response.choices[0].text)
+i = 1
+while 1:
+  js = json.dumps(all_messages[-(i+1):])
+  if count_tokens(js) > rem_tokens: break
+  i = i + 1
+
+messages = all_messages[-i:]
+l = count_tokens(json.dumps(messages))
+
+#
+# call the API...
 
 response = openai.ChatCompletion.create(
   model = model,
@@ -52,8 +71,11 @@ with open(fname_messages, 'a') as f:
   f.write(json.dumps(response.choices[0].message))
   f.write("\n")
 
-print('>>> ' + model + ' >>>')
+print(f">>> {model} i:{i} l:{l} >>>")
 print(response.choices[0].message.content)
-print('<<< ' + model + ' <<< .')
+print(f"<<< {model} <<< .")
 print()
+
+
+
 
