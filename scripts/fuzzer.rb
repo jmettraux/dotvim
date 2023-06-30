69cf91a0d9a8556240407c4aae85824cee747f6a
John Mettraux <jmettraux@gmail.com>
2023-06-30 19:54:54 +0900

    Track line in fuzzer

--- scripts/fuzzer.rb
+++ scripts/fuzzer.rb
@@ -8,6 +8,7 @@ rows, cols = IO.console.winsize
 
 fs = Dir['**/*.{js,rb,yaml,slim,scss,css,md,vim}'].sort
 fi = ''
+li = 1
 
 loop do
   fs1 =
@@ -15,8 +16,13 @@ loop do
     fs.select { |f| f.downcase.index(fi) }
   print "[2J"
   print"[2;1H"
-  fs1[0, rows - 2].each do |f|
-    print "[32m"
+  fs1[0, rows - 2].each_with_index do |f, i|
+    print "[0;0m"
+    if i == li
+      print "[1;32;7m"
+    else
+      print "[32m"
+    end
     puts f
   end
   print "[1;1H"
@@ -30,6 +36,10 @@ loop do
 
   if c == "\x7F"
     fi = fi[0..-2]
+  elsif c == "\e[A"
+    li = li - 1; li = 0 if li < 1
+  elsif c == "\e[B"
+    li = li + 1; li = fs1.length - 1 if li > fs1.length - 1
   elsif c.length > 1
     p c; sleep 0.5
   else
