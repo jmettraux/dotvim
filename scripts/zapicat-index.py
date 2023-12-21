bb318b323ef6ae2d5667bb75ec152853e9ca8ca4
John Mettraux <jmettraux@gmail.com>
2023-12-21 11:46:45 +0900

    Index non-class .js files

--- scripts/zapicat-index.py
+++ scripts/zapicat-index.py
@@ -2,20 +2,55 @@
 #
 # .vim/scripts/zapicat-index.py
 
-import os, re, sys, glob, json, hashlib
+import os, re, sys, glob
+#import os, re, sys, glob, json, hashlib
 
 #conf = 
 
-def shadig(x):
-  return hashlib.sha256(json.dumps(x).encode()).hexdigest()
+#def shadig(x):
+#  return hashlib.sha256(json.dumps(x).encode()).hexdigest()
 def readlines(path):
   return open(path, 'r').readlines()
 
 idx = { 'lines': {}, 'entries': [] }
   # TODO reload .zapicat.index
 
-RB_DEF_REX = re.compile(r'\bdef\s+([^\s(;]+)')
-RB_MOD_REX = re.compile(r'\b(module|class)\s*([^\s;{}]+)')
+JS_MOD_REX = re.compile(r'\b(class)\s+([a-zA-Z0-9_]+)')
+JS_MOJ_REX = re.compile(r'\b\s*var\s+([a-zA-Z0-9_]+)\s*=\s*\(function\(\)\s*\{')
+JS_DEF_REX = re.compile(r'\bthis\.([a-zA-Z0-9_]+)\s*=[^=]')
+JS_D3F_REX = re.compile(r'\bvar\s+([a-zA-Z0-9_]+)\s*=\s*function\(')
+  #
+def index_js_line(idx, path, line, l):
+  p = path + ':' + str(l)
+  m = re.search(JS_MOD_REX, line)
+  if m:
+    idx['lines'][p] = line.rstrip()
+    idx['entries'].append({
+      'l': 'js', 'p': p,  't': 'mod', 'k': m.group(2), 'tt': m.group(1) })
+  m = re.search(JS_MOJ_REX, line)
+  if m:
+    idx['lines'][p] = line.rstrip()
+    idx['entries'].append({
+      'l': 'js', 'p': p,  't': 'mod', 'k': m.group(1), 'tt': 'j' })
+  m = re.search(JS_DEF_REX, line)
+  if m:
+    idx['lines'][p] = line.rstrip()
+    idx['entries'].append({
+      'l': 'js', 'p': p,  't': 'def', 'k': m.group(1), 'tt': '-' })
+  m = re.search(JS_D3F_REX, line)
+  if m:
+    idx['lines'][p] = line.rstrip()
+    idx['entries'].append({
+      'l': 'js', 'p': p,  't': 'def', 'k': m.group(1), 'tt': 'p' })
+
+def index_js(idx, path):
+  l = 0
+  for line in readlines(path):
+    l = l + 1
+    index_js_line(idx, path, line, l)
+
+RB_DEF_REX = re.compile(r'\bdef\s+([a-zA-Z0-9.:]+)')
+RB_MOD_REX = re.compile(r'\b(module|class)\s*([a-zA-Z0-9.:]+)')
 #RB_ASS_REX = re.compile(r'\b([^=]+)\s*=\s*[^=>]')
   #
 def index_rb_line(idx, path, line, l):
@@ -38,7 +73,8 @@ def index_rb(idx, path):
     index_rb_line(idx, path, line, l)
 
 indexers = [
-  { 'rex': '\.rb$', 'fun': index_rb }
+  { 'rex': '\.rb$', 'fun': index_rb },
+  { 'rex': '\.js$', 'fun': index_js },
     ]
 
 def post_index(idx, index):
@@ -59,11 +95,14 @@ def index(idx, path):
 glo = sys.argv[1] if len(sys.argv) > 1 else '**/*'
 paths = glob.glob(glo, recursive=True)
 
+#
+# index
+
 for p in paths: index(idx, p)
 
-#for i in idx:
-#  #print(json.dumps(i))
-#  print(i['p'] + ':' + str(i['i']), i['t'], i['tt'], i['k'], '>', i['l'])
+#
+# output
+
 for e in idx['entries']:
   print(e['p'], e['t'], e['tt'], e['l'], e['k'])
 for l in idx['lines']:
