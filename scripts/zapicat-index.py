17eb58fea6451aac1fad4e58111b45b57627ebfe
John Mettraux <jmettraux@gmail.com>
2023-12-21 18:17:45 +0900

    Use json for .zapicat

--- scripts/zapicat-index.py
+++ scripts/zapicat-index.py
@@ -14,31 +14,10 @@ def to_list(x):
 def read_lines(path):
   return open(path, 'r').readlines()
 
-CONF_REX = re.compile(r'^\s*([-_.a-zA-Z0-9]+)\s*:\s*(.+)$')
-CONF_ARRAYS = [ 'exclude' ]
-  #
-def narrow_conf_value(v):
-  v = v.strip()
-  if v == 'true': return True
-  if v == 'false': return False
-  if v == 'nil': return None
-  if re.match(r'^\d+$', v): return int(v)
-  if re.search(r',', v): return list(map(narrow_conf_value, v.split(',')))
-  return v
-  #
 def read_conf():
-  h = {}
-  try:
-    for l in read_lines(CONF_FNAME):
-      m = re.match(CONF_REX, l)
-      if not(m): continue
-      k = m.group(1)
-      v = narrow_conf_value(m.group(2))
-      if k in CONF_ARRAYS: v = to_list(v)
-      h[k] = v
-  except:
-    1
-  return h
+  try: return json.load(open(CONF_FNAME, 'r'))
+  except: 1
+  return {}
 
 conf = read_conf()
 
