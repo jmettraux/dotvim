4fb1813d36c72afa4340702696f52aea36bba217
John Mettraux <jmettraux@gmail.com>
2026-07-20 16:06:31 +0900

    Add local llama-server complete

---
+++ scripts/local.vim
@@ -0,0 +1,75 @@
+
+" scripts/local.vim
+
+
+function! s:LocalChatComplete(prompt)
+
+  "let @z = system(g:_python . " ~/.vim/scripts/local_complete.py 2>/dev/null", a:prompt)
+  let @z = system(g:_python . " ~/.vim/scripts/local_complete.py 2>.local.stderr.txt", a:prompt)
+  silent $put z
+  normal <c-g>
+endfunction " LocalChatComplete
+
+
+function! s:LocalChatPushLine()
+
+  if getline('.') !~ '\v^#+\s+'
+    exe 'normal I### 0'
+  endif
+  let l = getline('.')
+  exe 'normal o'
+
+  redraw
+  write
+
+  call <SID>LocalChatComplete(l)
+  write
+endfunction " LocalChatPushLine
+
+"command! -nargs=0 Prompt :call <SID>LocalChatPushLine()
+
+
+function! s:LocalChatPushBlock() range
+
+  let ls = getline(a:firstline, a:lastline)
+
+  exe '' . a:firstline . ',' . a:lastline . 'delete'
+  call append(a:firstline - 1 , '### >')
+  call append(a:firstline + 0, '')
+    "
+  let ln = a:firstline + 1
+  for l in ls
+    if trim(l) == ''
+      call append(ln, '>')
+    else
+      call append(ln, '> ' . l)
+    endif
+    let ln = ln + 1
+  endfor
+  "call append(ln, '')
+
+  redraw
+  write
+
+  call <SID>LocalChatComplete(ls)
+  write
+endfunction " LocalChatPushBlock
+
+
+"function! s:OpenAiList()
+"
+"  let @z = system(g:_python . ' ~/.vim/scripts/openai_list.py')
+"  silent $put z
+"  normal <c-g>
+"endfunction " OpenAiList
+"
+"command! -nargs=0 OpenAiList :call <SID>OpenAiList()
+
+
+au BufReadPost,BufNewFile .local.md
+  \ nnoremap <buffer> ?? :call <SID>LocalChatPushLine()
+au BufReadPost,BufNewFile .local.md
+  \ vnoremap <buffer> ?? :call <SID>LocalChatPushBlock()
+au BufReadPost,BufNewFile .local.md
+  \ vnoremap <buffer> >> :call <SID>LocalChatPushBlock()
+
---
+++ scripts/local_complete.py
@@ -0,0 +1,152 @@
+
+#
+# scripts/local_complete.py
+
+import os, sys, re
+import requests, json
+from pathlib import Path
+
+#key_path = os.path.expanduser('~') + '/.vim/.deepseek.key.txt'
+
+
+# primitive but...
+#
+def count_tokens(s):
+  return \
+    len(re.findall(r'\w+', s)) + \
+    len(re.findall(r'\s', s)) + \
+    len(re.findall(r'[^\w\s]', s))
+
+role = 'user'
+
+url = 'http://192.168.1.222:8022/chat/completions'
+agent = 'vim-local-jmettraux'
+
+fname_last = '.local.last.json'
+fname_messages = '.local.messages.json'
+#model = 'deepseek-v4-flash'
+#model = 'deepseek-chat'
+#model = 'deepseek-reasoner'
+#model = 'deepseek-coder'
+model = "local"
+temperature = 1
+#temperature = 0.7
+
+whole_tokens = 8192
+max_tokens = 4096
+rem_tokens = whole_tokens - max_tokens
+
+lines = sys.stdin.read().strip()
+m = re.match(r'^(#+\s+)(.*)', lines)
+if m: lines = m.group(2)
+
+mod = ""
+m = re.match(r'([-a-z0-9]+)\s*:\s*(.*)', lines)
+if m: mod, lines = m.group(1).lower(), m.group(2)
+
+#if "reasoner" in mod:
+#  model = "deepseek-reasoner"
+#elif "coder" in mod:
+#  model = "deepseek-coder"
+    #
+    # well, it's always deepseek-v4-flash answering...
+
+prompt = { "role": role, "content": lines }
+
+Path(fname_messages).touch()
+
+all_messages = []
+  #
+with open(fname_messages) as f:
+  while line := f.readline():
+    line = line.strip()
+    if line == '': break
+    all_messages.append(json.loads(line))
+  #
+all_messages.append(prompt)
+
+i = 1
+l = len(all_messages)
+  #
+while 1:
+  js = json.dumps(all_messages[-(i+1):])
+  if count_tokens(js) > rem_tokens: break
+  i = i + 1
+  if i >= l: break
+
+messages = all_messages[-i:]
+#messages.insert(0, { "role": "user", "content": "ok" })
+l = count_tokens(json.dumps(messages))
+
+#
+# call the API...
+
+#api_key = None
+#with open(key_path, 'r') as file: api_key = file.read().strip()
+api_key = "nada"
+
+data = {
+  "model": model,
+  "messages": messages,
+  "temperature": temperature,
+  "max_tokens": max_tokens,
+  "stream": False }
+
+headers = {
+  'User-Agent': agent,
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer ' + api_key }
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
+suc = response.status_code == 200
+res = response.json() if suc else None
+cho = res['choices'][0]['message'] if suc else None
+
+with open(fname_last, 'w') as f:
+  headers['Authorization'] = 'Bearer {local-api-key}'
+  print('{ headers:', file=f)
+  print(json.dumps(headers, indent=2), file=f)
+  print(', postdata:', file=f)
+  print(json.dumps(data, indent=2), file=f)
+  print(', response:', file=f)
+  print(json.dumps(res, indent=2), file=f)
+  print('}', file=f)
+
+if suc:
+  with open(fname_messages, 'a') as f:
+    f.write(json.dumps(prompt, indent=None))
+    f.write("\n")
+    f.write(json.dumps(cho, indent=None))
+    f.write("\n")
+
+md = res['model']
+ct = res['usage']['completion_tokens'] if suc else -1
+pt = res['usage']['prompt_tokens'] if suc else -1
+#tt = res['usage']['total_tokens']
+
+print(f"  <!--  {model} .:. {md}  i:{i} l:{l} -- pt:{pt} ct:{ct}  -->")
+print()
+if suc:
+  content = cho['content']
+  reasoning = cho['reasoning_content']
+  if reasoning:
+    print()
+    print('  <!-- reasoning -->')
+    print()
+    #print(re.sub(r'\s+$', '', reasoning, flags=re.MULTILINE))
+    print(reasoning)
+    print('  <!-- / reasoning -->')
+    print()
+  #print(re.sub(r'\s+$', '', content, flags=re.MULTILINE))
+  print(content)
+else:
+  print('```')
+  print(f'HTTP status code: {response.status_code}')
+  print()
+  print(response.text)
+  print('```')
+print()
+print('<!-- . -->')
+#print(f"<!-- {url} -->")
+print()
+
